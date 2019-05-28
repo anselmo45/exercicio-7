38329c9025bd143ab1573ad8609f576632ad5f72
Anselmo Luiz <anselmoluizd@gmail.com>
2019-05-28 07:44:04 -0300

    exercicio 7

--- src/Quest1/Teste.java
+++ src/Quest1/Teste.java
@@ -3,10 +3,9 @@ package Quest1;
 
 public class Teste {
     public static void main(String[]args){
-        Pessoa p1=new Pessoa("Toninho","Rua: manicomio X numero:171","(66)9666-6999");
-        Fornecedor f1=new Fornecedor("Inri Cristo","Núcleo Rural Casa Grande - Rua 8MA","(61) 3404-0134",1.87,1.71);
-        Empregado e1=new Empregado("O proximo numero","É uma frase >.<","01110110 01100101 01101001 01101111 00100000 01100011 01101111 01101110 01100110 01100101 01110010 01101001 01110010 00100000 01101111 01110001 00100000 01100101 01110010 01100001 00111111 ",8888,9999,5);
-        
+        Pessoa p1=new Pessoa("Anselmo","Rua: Camilo - 1006","(38)3741-2445");
+        Fornecedor f1=new Fornecedor("Eletrocom","Rua Pio XII - 606","(38)988078969",2000,00);
+        Empregado e1=new Empregado("Anselmo","Rua Camilo","1006",2000,500,101);
         
         System.out.println(f1.obterSaldo());
         System.out.println(e1.calcularSalario());
